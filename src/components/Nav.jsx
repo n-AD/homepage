6d0cd319e47016b1459194c4bc9ce51cb1a8fcaa
Nick <ndenbleyker@gmail.com>
2025-06-04 22:00:16 -0400

    updated nav bar to have hamburger menu for small screens and mobile devices. plus some other style updates, mainly adding an underline when hoving over the rootlink. also general CSS modernizations and cleanups

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,57 +1,56 @@
 import { NavLink, useLocation } from "react-router-dom";
+import { useState } from "react";
 import styles from "../styles/componentStyles/nav.module.css";
-
 import logo from "../assets/images/audiodidacts-logo.png";
+
 const Nav = () => {
   const location = useLocation();
   const Paths = ["/Posts", "/Projects", "/About"];
   const isActive = Paths.some((path) => location.pathname.startsWith(path));
 
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+  const closeMenu = () => setIsMenuOpen(false);
+
   return (
-    <header
-      className={isActive ? `${styles.navbar} ${styles.sticky}` : styles.navbar}
+    <header className={`${styles.navbar} ${isActive ? styles.sticky : ""}`}>
+  <nav className={styles.headerLinksContainer}>
+    <div className={styles.navRoot}>
+      <NavLink to="/" className={styles.rootLink} onClick={closeMenu}>
+        <img src={logo} alt="Logo" className={styles.logo} />
+        Nick DenBleyker
+      </NavLink>
+    </div>
+
+    <div className={styles.navLinks}>
+      <NavLink to="/Posts" className={styles.navLink}>Posts</NavLink>
+      <NavLink to="/Projects" className={styles.navLink}>Projects</NavLink>
+      <NavLink to="/About" className={styles.navLink}>About</NavLink>
+    </div>
+
+    <div
+      className={`${styles.hamburger} ${
+        isMenuOpen ? styles.open : ""
+      }`}
+      onClick={toggleMenu}
     >
-      <nav className={styles.headerLinksContainer}>
-        <div className={styles.navRoot}>
-          <NavLink to="/" className={styles.rootLink}>
-            <img src={logo} alt="Audiodidacts logo" className={styles.logo} />
-            Nick DenBleyker
-          </NavLink>
-        </div>
-        <div className={styles.navLinks}>
-          <NavLink
-            to="/Posts"
-            className={
-              location.pathname === "/Posts"
-                ? `${styles.navLink} ${styles.active}`
-                : styles.navLink
-            }
-          >
-            Posts
-          </NavLink>
-          <NavLink
-            to="/Projects"
-            className={
-              location.pathname === "/Projects"
-                ? `${styles.navLink} ${styles.active}`
-                : styles.navLink
-            }
-          >
-            Projects
-          </NavLink>
-          <NavLink
-            to="/About"
-            className={
-              location.pathname === "/About"
-                ? `${styles.navLink} ${styles.active}`
-                : styles.navLink
-            }
-          >
-            About
-          </NavLink>
-        </div>
-      </nav>
-    </header>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  </nav>
+
+  {/* Move this outside of headerLinksContainer */}
+  {isMenuOpen && (
+    <div className={styles.mobileNav}>
+      <NavLink to="/Posts" className={styles.navLink} onClick={closeMenu}>Posts</NavLink>
+      <NavLink to="/Projects" className={styles.navLink} onClick={closeMenu}>Projects</NavLink>
+      <NavLink to="/About" className={styles.navLink} onClick={closeMenu}>About</NavLink>
+    </div>
+  )}
+</header>
+
   );
 };
 
